afa7d11405d1db4400a475ce93c2f3b5184e7ffb
limisie <k.limisiewicz@gmail.com>
2024-05-06 03:30:58 +0200

    feat: theme handling for Astro upgrade

---
+++ src/scripts/theme.js
@@ -0,0 +1,42 @@
+const themeButton = document.querySelector("#themeButton");
+const themeIcon = document.querySelector("#themeIcon");
+
+function getTheme() {
+    if (
+        typeof localStorage !== "undefined" &&
+        localStorage.getItem("theme")
+    ) {
+        return localStorage.getItem("theme");
+    }
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        return "dark";
+    }
+    return "light";
+};
+
+function changeTheme(newTheme) {
+    document.documentElement.dataset.theme = newTheme;
+    themeIcon.setAttribute("class", newTheme);
+}
+
+function handleThemeClick() {
+    const theme = document.documentElement.dataset.theme;
+    const newTheme = theme == "light" ? "dark" : "light";
+    changeTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+}
+
+document.addEventListener(
+    "astro:page-load",
+    () => {
+        const theme = getTheme()
+        changeTheme(theme);
+        localStorage.setItem("theme", theme);
+    },
+    { once: true },
+);
+
+themeButton.addEventListener("click", handleThemeClick);
+document.addEventListener("astro:after-swap", () => {
+    changeTheme(getTheme());
+});
